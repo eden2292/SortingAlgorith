7514c33958a59f18505581f2076c4bbb25856f8d
eden2292 <115145642+eden2292@users.noreply.github.com>
2022-10-14 15:35:14 +0100

    Add files via upload

---
+++ SortingAlgorith/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] firstArray = new int[] {5,2,1,3,4};
+int[] secondArray = new int[5];
+
+
+secondArray[0] = firstArray[0];
+
+foreach(var key in firstArray)
+{
+    if (firstArray[i] < secondArray[i-1])
+    {
+        secondArray[1] = secondArray[0];
+        secondArray[0] = firstArray[1];
+
+    }
+
+    else if ()
+    {
+
+    }
+    else
+    {
+        secondArray[1] = firstArray[1];
+    }
+
+}
